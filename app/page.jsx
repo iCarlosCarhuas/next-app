903bd9329450ec084f371b3d6693bb6c7f2b0955
iCarlosCarhuas <CRCT-2003@hotmail.com>
2023-05-09 18:25:45 -0500

    Third branch commit
    Fetching API user data

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,20 @@
-const IndexPage = () => {
+async function fetchUsers(){
+  
+  const res = await fetch("https://reqres.in/api/users")
+  const data = await res.json();
+  return data.data
+}
+
+
+async function IndexPage() {
+  const users = await fetchUsers()
   return (
-    <div>hello world</div>
+    <div>
+      Index Page
+      <div>
+        {JSON.stringify(users)}
+      </div>
+    </div>
     )
 }
 
